2b8001c3976ccf15c17a19cca018418d6ba8d9b8
EthanCarson <digitalec414@outlook.com>
2024-12-06 16:37:31 -0500

    Began homepage layout and Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-    <div className="Secondary">
-      <button>Click Me:</button>
+    <div id="Home">
+      <header>Header</header>
+      <section id="Intro">
+        <article>Image</article>
+        <article>
+          <h1>Ethan Carson</h1>
+          <p>Text</p>
+        </article>
+      </section>
+      <section className="secondary">
+        <h1>Personal Highlights:</h1>
+        <div id="Highlight1">
+          <img src="" alt="" />
+          <caption></caption>
+        </div>
+        <div id="Highlight2">
+          <img src="" alt="" />
+          <caption></caption>
+        </div>
+      </section>
     </div>
   );
 }
