522c0f4abd98f3c77ce4a7a5392ed34ec7939e36
EthanCarson <digitalec414@outlook.com>
2024-12-14 00:14:01 -0500

    Formatting Done for the Most part

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,17 @@
 import Header from "../components/Header";
 export default function Contact() {
-  <div id="Contact">
-    return <Header HImage="#" Heading="Ethan Carson" />;
-  </div>;
+  return (
+    <div id="Contact">
+      <Header HImage="#" Heading="Ethan Carson" />
+      <aside>
+        <img src="" alt="" />
+        <p>Stuff</p>
+      </aside>
+      <section>
+        <p>Contact Stuff</p>
+        <button>Send Email</button>
+        <button>Call Instead</button>
+      </section>
+    </div>
+  );
 }
