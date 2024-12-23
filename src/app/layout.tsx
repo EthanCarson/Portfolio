8f42fb6734c0e589a4ed8e15da7ec51bc9e66d4c
EthanCarson <digitalec414@outlook.com>
2024-12-23 12:59:13 -0500

    No Parallax but Navigation is working as expected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,25 +5,23 @@ import { ReactNode } from "react"; // Correctly import ReactNode
 import Head from "next/head";
 import { Press_Start_2P } from "next/font/google";
 import "../../styles/globals.css"; // Ensure the SASS file is imported
-import { motion, useTransform, useScroll } from "framer-motion";
+
 const pressStart2P = Press_Start_2P({
   weight: ["400"],
   subsets: ["latin"]
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const { scrollYProgress } = useScroll();
-  const parallaxEffect = useTransform(scrollYProgress, [0, 1], [0, 500]);
   return (
     <html lang="en" className={pressStart2P.className}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <motion.body style={{ y: parallaxEffect }}>
+      <body>
         <Navigation />
         {children}
         <Footer />
-      </motion.body>
+      </body>
     </html>
   );
 }
